5c3f363442cac738a9c57d13177e44c0e5068db1
Nisha <Nisha@192.168.1.102>
2015-04-23 14:55:56 -0600

    Changed name of files

--- GallToLit/src/gallonstoliters/GallonsToLiters.java
+++ GallToLit/src/gallonstoliters/GallonsToLiters.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package galltolit;
+package gallonstoliters;
 
 /**
  *
  * @author Nisha
  */
-public class GallToLit {
+public class GallonsToLiters {
 
     /**
      * @param args the command line arguments
